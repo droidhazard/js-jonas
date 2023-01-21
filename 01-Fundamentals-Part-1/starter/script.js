766e699cf57bd38916df30e7d8e0096dbd0503d5
Zohaib Ali <www.zohaib.0335@gmail.com>
2023-01-21 12:27:44 +0500

    learn

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,13 +1,22 @@
 let js = 'amazing'
-if (js === 'amazing') alert("Javascript is FUN")
 
-console.log(40 + 8 + 23 + 10)
+console.log('Jonas')
+console.log(23)
 
-const int1 = document.getElementById('int1')
-const int2 = document.getElementById('int2')
-const result = document.getElementById('result')
-const calc = document.getElementById('calc')
+let firstName = "Matilda"
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
 
-calc.onclick = () => {
-  result.innerText = Number.parseInt(int1.value) + Number.parseInt(int2.value);
-}
+let jonas_matilda = 'JM'
+let $function = 27
+const CONSTANT = 'hello'
+const link = 'bye'
+
+let myFirstJob = 'Programmer'
+let myCurrentJob = 'Teacher'
+
+let job1 = 'programmer'
+let job2 = 'teacher'
+
+console.log(myFirstJob)
