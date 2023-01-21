54ddc9b8a2e692140d2a4e14473c7fd4d1fb7578
Zohaib Ali <www.zohaib.0335@gmail.com>
2023-01-21 21:55:08 +0500

    learn

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,45 +1,54 @@
-/*
-let js = 'amazing'
-console.log(2 + 2)
+// let js = 'amazing'
+// console.log(2 + 2)
 
-console.log('Jonas')
-console.log(23)
+// console.log('Jonas')
+// console.log(23)
 
-let firstName = "Matilda"
-console.log(firstName)
-console.log(firstName)
-console.log(firstName)
+// let firstName = "Matilda"
+// console.log(firstName)
+// console.log(firstName)
+// console.log(firstName)
 
-// Variable name converntion
-let jonas_matilda = 'JM'
-let $function = 27
-const CONSTANT = 'hello'
-const link = 'bye'
+// // Variable name converntion
+// let jonas_matilda = 'JM'
+// let $function = 27
+// const CONSTANT = 'hello'
+// const link = 'bye'
 
-let myFirstJob = 'Programmer'
-let myCurrentJob = 'Teacher'
+// let myFirstJob = 'Programmer'
+// let myCurrentJob = 'Teacher'
 
-let job1 = 'programmer'
-let job2 = 'teacher'
+// let job1 = 'programmer'
+// let job2 = 'teacher'
 
-console.log(myFirstJob)
-*/
+// console.log(myFirstJob)
 
 
-let javascriptIsFun = true
-console.log(javascriptIsFun)
-console.log(typeof false)
-console.log(typeof javascriptIsFun)
-console.log(typeof 23)
-console.log(typeof 'Zohaib')
+// let javascriptIsFun = true
+// console.log(javascriptIsFun)
+// console.log(typeof false)
+// console.log(typeof javascriptIsFun)
+// console.log(typeof 23)
+// console.log(typeof 'Zohaib')
 
-javascriptIsFun = 'YES!'
-console.log(typeof javascriptIsFun)
+// javascriptIsFun = 'YES!'
+// console.log(typeof javascriptIsFun)
 
-let year;
-console.log(year)
-console.log(typeof year)
+// let year;
+// console.log(year)
+// console.log(typeof year)
 
-year = 1991
+// year = 1991
 
-console.log(typeof null)
+// console.log(typeof null)
+
+let age = 30
+age = 31
+const birthYear = 1991
+// birthYear = 2001
+// const job;
+var job = 'programmer'
+job = 'teacher'
+
+lastName = 'Ali'
+console.log(lastName)
