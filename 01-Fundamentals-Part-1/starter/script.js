16d7c1ee9e6209663610dec3e32b8c59c54802f1
Zohaib Ali <www.zohaib.0335@gmail.com>
2023-01-22 18:40:02 +0500

    learn

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -53,22 +53,71 @@
 // lastName = 'Ali'
 // console.log(lastName)
 
-const now = 2037
-const ageZohaib = now - 2001
-const ageSarah = now - 2018
-console.log(ageZohaib, ageSarah)
+// const now = 2037
+// const ageZohaib = now - 2001
+// const ageSarah = now - 2020
+// console.log(ageZohaib, ageSarah)
 
-console.log(ageZohaib * 2, ageZohaib / 10, 2 ** 3)
-// 2 ** 3 mean 2 to the power of 3 , = 2 * 2 * 2
+// console.log(ageZohaib * 2, ageZohaib / 10, 2 ** 3)
+// // 2 ** 3 mean 2 to the power of 3 , = 2 * 2 * 2
+
+// const firstName = 'Zohaib'
+// const lastName = 'Ali'
+// console.log(firstName + " " + lastName)
+
+// let x = 10 + 5
+// x += 10  // x = x + 10
+// x *= 4
+// x++
+// x--;x--
+// console.log(x)
+
+// console.log(ageZohaib > ageSarah)
+// console.log(ageSarah >= 18)
+
+// const isFullAge = ageSarah >= 18
+
+// console.log(now - 2001 > now - 2020)
+
+// const now = 2037
+// const ageZohaib = now - 2001
+// const ageSarah = now - 2018
+// console.log(now - 2001 > now - 2018)
+
+// console.log(25 - 10 - 5)
+
+// let x, y
+// x = y = 25 - 10 - 5
+// console.log(x, y)
+
+// const averageAge = (ageZohaib + ageSarah) / 2
+// console.log(averageAge)
+
+// const markMass_test1 = 78
+// const markHeight_test1  = 1.69
+// const johnMass_test1 = 92
+// const johnHeight_test1 = 1.95
+
+// const markBMI_test1 = markMass_test1 / (markHeight_test1 ** 2)
+// const johnBMI_test1 = johnMass_test1 / (johnHeight_test1 ** 2)
+
+// const markHigherBMI_test1 = markBMI_test1 > johnBMI_test1
+// console.log(markBMI_test1, johnBMI_test1)
+// console.log('is mark BMI higher than john ? ', markHigherBMI_test1)
+
+// const markMass_test2 = 95
+// const markHeight_test2  = 1.88
+// const johnMass_test2 = 85
+// const johnHeight_test2 = 1.76
+
+// const markBMI_test2 = markMass_test2 / (markHeight_test2 ** 2)
+// const johnBMI_test2 = johnMass_test2 / (johnHeight_test2 ** 2)
+
+// const markHigherBMI_test2 = markBMI_test2 > johnBMI_test2
+// console.log(markBMI_test2, johnBMI_test2)
+// console.log('is mark BMI higher than john ? ', markHigherBMI_test2)
 
 const firstName = 'Zohaib'
-const lastName = 'Ali'
-console.log(firstName + " " + lastName)
-
-let x = 10 + 5
-x += 10  // x = x + 10
-x *= 4
-x++
-x--;x--
-console.log(x)
+const job = 'farigh'
+const birthYear = 2001
 
