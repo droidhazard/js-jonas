35b05bdcea01b5ad624a03297cf95a7556047b92
Zohaib Ali <www.zohaib.0335@gmail.com>
2023-01-24 12:53:08 +0500

    learn

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -203,7 +203,22 @@
 //   console.log('noo! height is UNDEFINED')
 // }
 
-const age = 18
-if (age === 18) {
-  
+const age = '18'
+if (age === 18) console.log('You just became an adult (strict)')
+
+if (age == 18) console.log('You just became an adult (loose)')
+
+const favorite = prompt("What's your favorite number?")
+console.log(typeof favorite)
+
+if (Number(favorite) === 23) {
+  console.log('Cool! 23 is an amazing number1')
+} else if (Number(favorite) === 7 ) {
+  console.log('7 is also a cool number')
+} else if (Number(favorite) === 9 ) {
+  console.log('9 is also a cool number')
+} else {
+  console.log('Number is not 23, 7 or 9')
 }
+
+if (Number(favorite) !== 23) console.log('Why not 23 ?')
