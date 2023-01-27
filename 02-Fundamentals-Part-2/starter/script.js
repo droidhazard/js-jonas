74beac0b2800656652b38f49061ccb1e858f9ccf
Zohaib Ali <www.zohaib.0335@gmail.com>
2023-01-27 12:27:06 +0500

    arrow functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -31,19 +31,34 @@
 // const appleOrangeJuice = fruitProcessor(2, 4)
 // console.log(appleOrangeJuice)
 
-// Function Declaration
-function calcAge1(birthYear) {
-  return 2023 - birthYear;
+// // Function Declaration
+// function calcAge1(birthYear) {
+//   return 2023 - birthYear;
+// }
+
+// const age1 = calcAge1(2001)
+
+// // Function Expression
+// const calcAge2 = function (birthYear) {
+//   return 2023 - birthYear;
+// }
+
+// const age2 = calcAge2(2001)
+
+// console.log(age1, age2)
+
+// Arrow function
+
+const calcAge3 = birthYear => 2037 - birthYear
+const age3 = calcAge3(2004)
+console.log(age3)
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear
+  const retirement = 65 - age
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`
 }
 
-const age1 = calcAge1(2001)
-
-// Function Expression
-const calcAge2 = function (birthYear) {
-  return 2023 - birthYear;
-}
-
-const age2 = calcAge2(2001)
-
-console.log(age1, age2)
-
+console.log(yearsUntilRetirement(2002, 'zohaib'))
+console.log(yearsUntilRetirement(2001, 'droid'))
