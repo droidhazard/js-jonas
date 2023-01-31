626be66fe4f97eff14f25f28fef0b9a23f4f60cb
Zohaib Ali <www.zohaib.0335@gmail.com>
2023-01-31 07:59:44 +0500

    backward loops, loops inside loops

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -313,48 +313,70 @@
 //   console.log(`Lifting weight repetition ${rep}`)
 // }
 
+// const zohaibArray = [
+//   'Zohaib',
+//   'Ali',
+//   2037 - 2001,
+//   'coder',
+//   ['Michael', 'Peter', 'Steven'],
+//   true
+// ]
+
+// let arrayTypes = []
+
+// for (let i = 0; i < zohaibArray.length; i++) {
+//   console.log(zohaibArray[i], typeof zohaibArray[i])
+//   // arrayTypes.push(typeof zohaibArray[i])
+//   arrayTypes[i] = typeof zohaibArray[i]
+// }
+// console.log(arrayTypes)
+
+// const years = [1991, 2007, 1969, 2020]
+// const ages = []
+
+// for (let i = 0; i < years.length; i++) {
+//   ages.push(2037 - years[i])
+// }
+// console.log(years)
+// console.log(ages)
+
+// // continue and break
+// console.log('-------ONLY STRINGS-------')
+// for (let i = 0; i < zohaibArray.length; i++) {
+//   if (typeof zohaibArray[i] === 'string') {
+//     console.log(zohaibArray[i], typeof zohaibArray[i])
+//   } else {
+//     continue
+//   }
+// }
+
+// console.log('-------BREAK WITH NUMBER-------')
+// for (let i = 0; i < zohaibArray.length; i++) {
+//   if (typeof zohaibArray[i] === 'number') {
+//     console.log(zohaibArray[i], typeof zohaibArray[i])
+//   } else {
+//     continue
+//   }
+// }
+
 const zohaibArray = [
   'Zohaib',
   'Ali',
   2037 - 2001,
   'coder',
   ['Michael', 'Peter', 'Steven'],
-  true
+  true,
+  false
 ]
 
-let arrayTypes = []
-
-for (let i = 0; i < zohaibArray.length; i++) {
-  console.log(zohaibArray[i], typeof zohaibArray[i])
-  // arrayTypes.push(typeof zohaibArray[i])
-  arrayTypes[i] = typeof zohaibArray[i]
-}
-console.log(arrayTypes)
-
-const years = [1991, 2007, 1969, 2020]
-const ages = []
-
-for (let i = 0; i < years.length; i++) {
-  ages.push(2037 - years[i])
-}
-console.log(years)
-console.log(ages)
-
-// continue and break
-console.log('-------ONLY STRINGS-------')
-for (let i = 0; i < zohaibArray.length; i++) {
-  if (typeof zohaibArray[i] === 'string') {
-    console.log(zohaibArray[i], typeof zohaibArray[i])
-  } else {
-    continue
-  }
+for (let i = zohaibArray.length - 1; i >= 0; i--) {
+  console.log(i, zohaibArray[i])
 }
 
-console.log('-------BREAK WITH NUMBER-------')
-for (let i = 0; i < zohaibArray.length; i++) {
-  if (typeof zohaibArray[i] === 'number') {
-    console.log(zohaibArray[i], typeof zohaibArray[i])
-  } else {
-    continue
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`----------- Starting exercise ${exercise}`)
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`)
   }
 }
