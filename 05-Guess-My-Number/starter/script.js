029bd8c7d501a58a264456b9b0bf250fb8bc4a6d
Zohaib Ali <www.zohaib.0335@gmail.com>
2023-02-08 19:14:54 +0500

    content manipulation basics

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,4 +1,15 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent)
-document.querySelector('.message').textContent = 'why'
+// document.querySelector('.message').textContent = 'why'
+document.querySelector('.message').textContent = 'Correct Number!'
+console.log(document.querySelector('.message').textContent)
+
+console.log(document.querySelector('.number').textContent)
+console.log(document.querySelector('.score').textContent)
+
+document.querySelector('.number').textContent = 20
+document.querySelector('.score').textContent = 13
+
+document.querySelector('.guess').value = 11
+console.log(document.querySelector('.guess').value)
