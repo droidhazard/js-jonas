5c38550a3929cff0bc01965b625d42dd2928a98c
Zohaib Ali <www.zohaib.0335@gmail.com>
2023-02-14 19:36:12 +0500

    show and hide modal using buttons

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -7,9 +7,25 @@ const btnsOpenModal = document.querySelectorAll('.show-modal')
 
 console.log(btnsOpenModal)
 
-for (let i = 0; i < btnsOpenModal.length; i++){
-  console.log(btnsOpenModal[i])
-  btnsOpenModal[i].addEventListener('click', ()=> {
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  console.log(btnsOpenModal[i].textContent)
+  btnsOpenModal[i].addEventListener('click', () => {
+    console.log("Button clicked")
+    console.log(btnsOpenModal[i].textContent)
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+  })
 
+  document.body.addEventListener('keypress', (e) => {
+    console.log(e)
   })
 }
+
+overlay.addEventListener('click', () => {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+})
+btnCloseModal.addEventListener('click', () => {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+})
